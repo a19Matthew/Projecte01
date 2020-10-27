7098d4d67008bddbe0dbf43517aed7c88dc189c3
Matthew <a19matrivort@inspedralbes.cat>
2020-10-27 11:49:48 -0400

    0.2

--- Projecte01.sh
+++ Projecte01.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 #Aquest es el nou projecte en github
+
+if [[ $EUID -ne 0  ]]; then
+	echo "Has d'obrir aquest script com a sudo"
+	exit 1
+fi
+
+echo "Introdueix el nom d'usuari: "
+
+read nomusuari
+
+echo "Introdueix el teu nom real: "
+
+read nomreal
+
+echo "Introdueix la teva contrassenya: "
+
+read -s cont
+
+OUTPUT=`useradd $nomusuari | tail -n 1`
+
+echo $OUTPUT
