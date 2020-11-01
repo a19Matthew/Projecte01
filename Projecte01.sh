885a3cc28a100d2ad69ad09d31a7e737d279353f
Matthew <a19matrivort@inspedralbes.cat>
2020-11-01 07:08:47 -0500

    0.3

--- Projecte01.sh
+++ Projecte01.sh
@@ -21,4 +21,18 @@ read -s cont
 
 OUTPUT=`useradd $nomusuari | tail -n 1`
 
-echo $OUTPUT
+LastUser=`cat /etc/passwd | tail -n 1 | cut -d ':' -f 1`
+
+if [[ $LastUser -eq $nomusuari ]]; then
+	echo 'Usuari creat satisfactoriament'
+else
+	echo 'Error en la creació del usuari'
+fi
+
+echo -e $cont | passwd --stdin $nomusuari
+
+passwd --expire $nomusuari
+
+nomhost=`hostname`
+
+echo 'Nom usuari: '$nomusuari', Contrassenya: '$cont', Nom del host: '$nomhost
